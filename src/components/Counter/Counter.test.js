8d7ec35a0f67bc8476e009c364c4098e5f74a7ac
nyhvignoli <nyhv.contato@gmail.com>
2022-02-14 14:15:58 -0300

    add decrease counter unit tests

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -42,4 +42,28 @@ describe("Counter", () => {
 
     expect(counter).toHaveTextContent('4')
   })
+
+  it("should decrease the counter value by clicking on minus button", () => {
+    render(<Counter />)
+    const minusButton = screen.getByRole('button', { name: '-' })
+    const counter = screen.getByText('0')
+    const plusButton = screen.getByRole('button', { name: '+' })
+
+    expect(minusButton).toBeDisabled()
+    expect(plusButton).not.toBeDisabled()
+
+    userEvent.dblClick(plusButton)
+
+    expect(counter).toHaveTextContent('2')
+    expect(minusButton).not.toBeDisabled()
+
+    userEvent.click(minusButton)
+
+    expect(counter).toHaveTextContent('1')
+
+    userEvent.click(minusButton)
+
+    expect(counter).toHaveTextContent('0')
+    expect(minusButton).toBeDisabled()
+  })
 })
